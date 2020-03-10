8cf75b6565e7649c86c1ae514f86e758a57fe41f
fiazashraf <fiazashraf73@gmail.com>
2020-03-10 16:09:14 +0500

    project created

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+
+    <View style={{
+      flex:1,
+      backgroundColor:'#faf'
+    }}>
+      <Text>
+        Study
+      </Text>
+    </View>
+
+  );
+};
+
+
+export default App;
